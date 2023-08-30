94165775346de958677b58ed7ccb5b0f995e9f72
Mtmozart <mmsnborges@gmail.com>
2023-08-29 22:15:49 -0300

    EnvConfigServices unit tests

---
+++ src/shared/infrastructure/env-config/__test__/unit/env-config.service.spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { EnvConfigService } from '../../env-config.service'
+import { EnvConfigModule } from '../../env-config.module'
+
+describe('EnvConfigService unit tests ', () => {
+  let sut: EnvConfigService
+  /*Exite uma convenção que defino a classe do teste ou variável principal que diz sut*/
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [EnvConfigModule.forRoot()],
+      providers: [EnvConfigService],
+    }).compile()
+
+    sut = module.get<EnvConfigService>(EnvConfigService)
+  })
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined()
+  })
+  it('should return the variable PORT', () => {
+    expect(sut.getAppPort()).toBe(3000)
+  })
+  it('should return the variable NODE_ENV', () => {
+    expect(sut.getNodeEnv()).toBe('test')
+  })
+})
